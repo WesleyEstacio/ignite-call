270071bc7243f933058da00528bf7e9ac52ce6fd
Wesley Estacio <wesleyestacio4@gmail.com>
2023-02-15 21:19:00 -0300

    feat: Landing page

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -20,7 +20,9 @@ export default function Document() {
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
-        ></style>
+        />
+
+        <title>Agendamento Simplificado</title>
       </Head>
       <body>
         <Main />
