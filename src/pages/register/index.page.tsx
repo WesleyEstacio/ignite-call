0eaaa51aa9bd6cadbcfe839bf8db0353f928dfdb
Wesley Estacio <wesleyestacio4@gmail.com>
2023-02-16 20:45:44 -0300

    feat: Register Form Validate Inputs

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,38 +1,88 @@
-import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
-import { ArrowRight } from "phosphor-react";
-import { Container, Form, Header } from "./styles";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
+import { ArrowRight } from 'phosphor-react'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Container, Form, FormError, Header } from './styles'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode conter apenas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
+
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras.' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export default function Register() {
-    return (
-        <Container>
-            <Header>
-                <Heading as='strong'>
-                    Bem-vindo ao Ignite Call!
-                </Heading>
-                <Text>Precisamos de algumas informações para criar seu perfil! Ah, você pode editar essas informações depois.</Text>
-                <MultiStep size={4} currentStep={1} />
-            </Header>
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
 
-            <Form
-                as='form'
-            >
-                <label>
-                    <Text size={'sm'}>Nome de usuário</Text>
-                    <TextInput prefix="agendar.com/" placeholder="seu-usuario"/>
-                </label>
+  const router = useRouter()
 
-                <label>
-                    <Text size={'sm'}>Nome completo</Text>
-                    <TextInput placeholder="Seu nome"/>
-                </label>
+  useEffect(() => {
+    if (router.query?.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
-                <Button
-                    type="submit"
-                >
-                    Próximo passo
-                    <ArrowRight />
-                </Button>
-            </Form>
-        </Container>
-    )
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+        <label>
+          <Text size={'sm'}>Nome de usuário</Text>
+          <TextInput
+            prefix="agendar.com/"
+            placeholder="seu-usuario"
+            {...register('username')}
+          />
+
+          {errors.username && (
+            <FormError size={'sm'}>{errors.username.message}</FormError>
+          )}
+        </label>
+
+        <label>
+          <Text size={'sm'}>Nome completo</Text>
+          <TextInput placeholder="Seu nome" {...register('name')} />
+          {errors.name && (
+            <FormError size={'sm'}>{errors.name.message}</FormError>
+          )}
+        </label>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
 }
