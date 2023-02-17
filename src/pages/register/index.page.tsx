c4e5099abe39bed0c431ff7e7ea886cc9d7a7dec
Wesley Estacio <wesleyestacio4@gmail.com>
2023-02-16 21:48:00 -0300

    feat: User Register

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,3 +1,4 @@
+import { api } from '@/lib/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { useRouter } from 'next/router'
@@ -6,6 +7,7 @@ import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Container, Form, FormError, Header } from './styles'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -42,7 +44,19 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message )
+        return
+      }
+
+      console.error(err)
+    }
   }
 
   return (
